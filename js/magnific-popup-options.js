e1f8598529b6692215a029039ff0b61a8f520e57
Mario Beck <22878918+marioisbeck@users.noreply.github.com>
2023-02-18 01:09:19 +0100

    updated story and tried to add correct youtube video

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -28,16 +28,15 @@ $(document).ready(function() {
 
 	var magnifVideo = function() {
 		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
-        fixedContentPos: false
+		fixedContentPos: false
     });
 	};
-
 	
 
 
